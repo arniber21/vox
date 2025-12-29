fc777f6a4eecab45c8234a39fe4fe4d5e6539de5
Arnab Ghosh <arnabcare21@gmail.com>
2025-12-29 00:56:34 -0800

    Disable SSL for DB

--- vox/src/payload.config.ts
+++ vox/src/payload.config.ts
@@ -77,6 +77,7 @@ export default buildConfig({
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
+      ssl: { rejectUnauthorized: false },
     },
   }),
   collections: [Pages, Users, Labs, Journals, Articles, Media],
